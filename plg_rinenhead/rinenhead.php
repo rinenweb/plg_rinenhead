4bc4ed0eabb7707a4e2e6c0c78b82df5247fbbcc
Ioannis Fytros <rinenweb@gmail.com>
2024-04-16 17:38:19 +0300

    new functionality
    Adds another field where you can insert some js code to be put just before the end of the body section.

--- plg_rinenhead/rinenhead.php
+++ plg_rinenhead/rinenhead.php
@@ -9,8 +9,11 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Factory;
+
 
 class PlgSystemRinenhead extends CMSPlugin
+  
 {
     public function onBeforeCompileHead()
     {
@@ -18,11 +21,27 @@ class PlgSystemRinenhead extends CMSPlugin
         
         // Only run this in the front end
         if ($app->isClient('site')) {
-            $doc = JFactory::getDocument();
+            $doc = Factory::getDocument();
             $customHtml = $this->params->get('customhtml', '');
             
             if (!empty($customHtml)) {
                 $doc->addCustomTag($customHtml);
+            }
+        }
+    }
+	public function onAfterRender()
+    {
+        $app = Factory::getApplication();
+
+        // Only run this in the front end
+        if ($app->isClient('site')) {
+            $body = $app->getBody();
+            $customJs = $this->params->get('customjs', '');
+
+            if (!empty($customJs)) {
+                // Append the custom JavaScript before the closing </body> tag
+                $body = str_replace('</body>', "<script type='text/javascript'>\n$customJs\n</script>\n</body>", $body);
+                $app->setBody($body);
             }
         }
     }
