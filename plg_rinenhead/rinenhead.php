f055514caed14d0cb046a18d8fda1b52bae734d9
Ioannis Fytros <rinenweb@gmail.com>
2026-07-20 11:59:11 +0300

    Refactor RinenHead plugin to implement SubscriberInterface

--- plg_rinenhead/rinenhead.php
+++ plg_rinenhead/rinenhead.php
@@ -1,7 +1,8 @@
 <?php
 /**
  * System Plugin for Joomla! - RinenHead
- *
+ * Injects custom markup into the document <head> and before the closing
+ * </body> tag on frontend HTML pages.
  * @author  Ioannis Fytros <info@rinenweb.eu>
  * @license GNU GPL v3 or later
  */
@@ -10,11 +11,29 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\Event\Event;
+use Joomla\Event\SubscriberInterface;
 
-class PlgSystemRinenhead extends CMSPlugin
+class PlgSystemRinenhead extends CMSPlugin implements SubscriberInterface
 {
 
     protected $autoloadLanguage = true;
+
+    /**
+     * Declare the events this plugin listens to.
+     *
+     * This replaces Joomla's deprecated auto-discovery of public onXxx()
+     * methods (which relies on reflection) while keeping the classic
+     * single-file plugin layout. Supported since Joomla 4.0.
+     * @return  array
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'onBeforeCompileHead' => 'onBeforeCompileHead',
+            'onAfterRender'       => 'onAfterRender',
+        ];
+    }
 
     private function getApp()
     {
@@ -31,14 +50,14 @@ class PlgSystemRinenhead extends CMSPlugin
 
     /**
      * Add custom markup to the document head.
-     *
-     * @return void
+     * @param   Event  $event  The dispatched event.
+     * @return  void
      */
-    public function onBeforeCompileHead()
+    public function onBeforeCompileHead(Event $event): void
     {
         $app = $this->getApp();
 
-        if (method_exists($app, 'isClient') && !$app->isClient('site')) {
+        if (!$app->isClient('site')) {
             return;
         }
 
@@ -48,9 +67,9 @@ class PlgSystemRinenhead extends CMSPlugin
             return;
         }
 
-        $document = method_exists($app, 'getDocument') ? $app->getDocument() : Factory::getDocument();
+        $document = $app->getDocument();
 
-        if (method_exists($document, 'getType') && $document->getType() !== 'html') {
+        if ($document->getType() !== 'html') {
             return;
         }
 
@@ -59,14 +78,14 @@ class PlgSystemRinenhead extends CMSPlugin
 
     /**
      * Add custom markup before the closing body tag.
-     *
-     * @return void
+     * @param   Event  $event  The dispatched event.
+     * @return  void
      */
-    public function onAfterRender()
+    public function onAfterRender(Event $event): void
     {
         $app = $this->getApp();
 
-        if (method_exists($app, 'isClient') && !$app->isClient('site')) {
+        if (!$app->isClient('site')) {
             return;
         }
 
@@ -79,7 +98,12 @@ class PlgSystemRinenhead extends CMSPlugin
         $body = $app->getBody();
 
         if (stripos($body, '</body>') !== false) {
-            $body = preg_replace('/<\/body\s*>/i', "\n" . $customJs . "\n</body>", $body, 1);
+            $replaced = preg_replace('/<\/body\s*>/i', "\n" . $customJs . "\n</body>", $body, 1);
+
+            // preg_replace returns null on failure; only overwrite on success.
+            if ($replaced !== null) {
+                $body = $replaced;
+            }
         } else {
             $body .= "\n" . $customJs;
         }
